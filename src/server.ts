efd33f4fbc6bc885b95dd31285f935b168c7daf5
Fernando Goncalves <fernando.hmg@gmail.com>
2021-04-21 16:42:43 -0300

    adding TypeORM, create Setting entity, migration

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express, { response } from "express";
+import express from "express";
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Get page" });
-});
+app.use(express.json());
 
-app.post("/", (request, response) => {
-  return response.json({ message: "Post page" });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("server running on 3333"));
